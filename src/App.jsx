b3008a00954a4858b02a853c7436c45d3e96e058
kamrul islam <mdkamrulislam9810@gmail.com>
2023-07-27 11:08:00 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@
 import React from 'react'
 import Student from './components/Student/Student'
 import Navbar from './components/Navbar/Navbar'
-import Singlestudent from './components/Student/Singlestudent'
 import AddStudent from './components/Student/AddStudent'
 import Contact from './components/Contact'
-import Chart from './components/ChartAll'
+
 import Error from './components/Error'
-import Drop from './components/Drop'
+
 import { Routes, Route, Router } from 'react-router-dom'
 
 const App = () => {
@@ -19,7 +18,6 @@ const App = () => {
         <Route path='/add-student' element={<AddStudent />} />
         <Route index element={<Student />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/chart' element={<Chart />} />
         <Route path='*' element={<Error />} />
       
 
